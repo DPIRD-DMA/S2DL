d9d04797fd7071e3dade347cf6345f3509239158
Nick <wrignj08@gmail.com>
2023-07-14 15:44:08 +0800

    working on Pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 setup(
     name="s2dl",
-    version="0.1",
+    version="0.2",
     url="https://github.com/DPIRD-DMA/S2DL",
     author="Nick Wright",
     author_email="nicholas.wright@dpird.wa.gov.au",
-    description="Description of my package",
+    description="S2DL is a Python library for downloading Sentinel-2 L1C satellite imagery data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[],
 )
